c04c18fddd06ffd385ce6fd3a168104ad0e7daa4
Brooke Kullberg, Darrion Gering and Marc Davies <marc@>
2019-07-03 16:10:07 -0700

    add newTicketForm component and route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './Header'
 import TicketList from './TicketList'
+import NewTicketForm from './NewTicketForm'
 import { Switch, Route } from 'react-router-dom'
 
 function App(){
@@ -9,6 +10,7 @@ function App(){
       <Header/>
       <Switch>
         <Route exact path='/' component={TicketList} />   
+        <Route path='/newticket' component={NewTicketForm} />
       </Switch>
     </div>
   )
---
+++ src/components/NewTicketForm.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function NewTicketForm(){
+  return (
+    <div>
+      <form>
+        <input
+          type='text'
+          id='names'
+          placeholder='Pair Names'
+          />
+          <br/>
+        <input
+          type='text'
+          id='location'
+          placeholder='Location'/>
+          <br/>
+        <textarea
+          id='issue'
+          placeholder='Describe your issue.'/>
+          <br/>
+        <button type='submit'>Help!</button>
+      </form>
+    </div>
+  );
+}
+
+export default NewTicketForm;
