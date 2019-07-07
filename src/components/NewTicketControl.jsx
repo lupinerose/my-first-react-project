4a911ca4647cc63daaca24f669d030618b230038
Brooke Kullberg <brookekullberg@gmail.com>
2019-07-07 13:38:10 -0700

    add NewTciketControl component

---
+++ src/components/NewTicketControl.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NewTicketControl extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>This is the NewTicketControl component!</p>
+      </div>
+    );
+  }
+}
+
+export default NewTicketControl;
