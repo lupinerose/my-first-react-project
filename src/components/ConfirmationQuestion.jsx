3c0a392eec7a26e5cc9fd685ffde664ecddb8049
Brooke Kullberg <brookekullberg@gmail.com>
2019-07-07 14:47:36 -0700

    add confimationQuestion component

---
+++ src/components/ConfirmationQuestion.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ConfirmationQuestions() {
+  return (
+    <div>
+      <p>Have you gone through all the steps on the Learn How To Program debugging lesson?</p>
+      <button>Yes</button>
+    </div>
+  );
+}
+
+export default ConfirmationQuestions;
