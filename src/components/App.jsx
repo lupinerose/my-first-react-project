d6f0b05e3efa4eecddebcd720fe8afabf5efd90b
Brooke Kullberg <brookekullberg@gmail.com>
2019-07-07 19:17:02 -0700

    fix error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,39 @@
-import React from 'react'
-import Header from './Header'
-import TicketList from './TicketList'
-import NewTicketControl from './NewTicketControl'
-import { Switch, Route } from 'react-router-dom'
-import Error404 from './Error404'
+import React from 'react';
+import Header from './Header';
+import TicketList from './TicketList';
+import NewTicketControl from './NewTicketControl';
+import Error404 from './Error404';
+import { Switch, Route } from 'react-router-dom';
 
 class App extends React.Component {
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       masterTicketList: []
-    }
-    this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this)
+    };
+    this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
   }
 
   handleAddingNewTicketToList(newTicket){
-    var newMasterTicketList = this.state.masterTicketList.slice()
-    newMasterTicketList.push(newTicket)
-    this.setState({masterTicketList: newMasterTicketList})
+    var newMasterTicketList = this.state.masterTicketList.slice();
+    newMasterTicketList.push(newTicket);
+    this.setState({masterTicketList: newMasterTicketList});
   }
 
-  render() {
-    return(
+  render(){
+    return (
       <div>
         <Header/>
         <Switch>
-          <Route exact path='/' render={()=><TicketList ticketList={this.state.masterTIcketList}/>}/>   
-          <Route path='/newticket' render={()=><NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList}/>} />
-          <Route component={Error404}/>
+          <Route exact path='/' render={()=><TicketList ticketList={this.state.masterTicketList} />} />
+          <Route path='/newticket' render={()=><NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList} />} />
+          <Route component={Error404} />
         </Switch>
       </div>
-    )
+    );
   }
+
 }
 
-export default App
+export default App;
--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import Ticket from './Ticket'
-import PropTypes from 'prop-types'
+import React from 'react';
+import Ticket from './Ticket';
+import PropTypes from 'prop-types';
 
 function TicketList(props){
-  console.log(props.ticketList);
-  return(
+  return (
     <div>
       <hr/>
-      {props.ticketList.map((ticket) => 
+      {props.ticketList.map((ticket) =>
         <Ticket names={ticket.names}
           location={ticket.location}
           issue={ticket.issue}
           key={ticket.id}/>
       )}
     </div>
-  )
+  );
 }
 
 TicketList.propTypes = {
   ticketList: PropTypes.array
-}
+};
 
-export default TicketList
+export default TicketList;
