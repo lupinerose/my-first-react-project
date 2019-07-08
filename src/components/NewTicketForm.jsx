367a575a6740705bcd8102d20254057ed3a85ca4
Brooke Kullberg <brookekullberg@gmail.com>
2019-07-07 17:56:39 -0700

    add handleNewTicketFormSubmission function

--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
 function NewTicketForm(){
+  let _names = null;
+  let _location = null;
+  let _issue = null;
 
   function handleNewTicketFormSubmission(event) {
     event.preventDefault();
+    console.log(_names.value);
+    console.log(_location.value);
+    console.log(_issue.value);
+    _names.value = '';
+    _location.value = '';
+    _issue.value = '';
   }
   return (
     <div>
@@ -11,14 +20,17 @@ function NewTicketForm(){
         <input
           type='text'
           id='names'
-          placeholder='Pair Names'/><br/>
+          placeholder='Pair Names'
+          ref={(input) => {_names = input;}}/><br/>
         <input
           type='text'
           id='location'
-          placeholder='Location'/><br/>
+          placeholder='Location'
+          ref={(input) =>{_location = input;}}/><br/>
         <textarea
           id='issue'
-          placeholder='Describe your issue.'/><br/>
+          placeholder='Describe your issue.'
+          ref={(textarea) => {_issue = textarea;}}/><br/>
         <button type='submit'>Help!</button>
       </form>
     </div>
